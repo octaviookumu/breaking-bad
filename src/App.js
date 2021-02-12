d66b1a99ac83a717f2ef2f67303b7caa89616cb6
Octavian <octavian@Christines-MacBook-Air.local>
2021-02-12 13:10:00 +0300

    Setting up hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./components/ui/Header";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      setItems(result.data);
+      setIsloading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="App">
       <Header />
