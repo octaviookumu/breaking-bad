bb37c6aa555d81899badba7739d1d0e403e70e99
Octavian <octavian@Christines-MacBook-Air.local>
2021-02-12 14:55:18 +0300

    Concluding by passing function from child to parent. Tough work.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsloading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios.get(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
       setItems(result.data);
       setIsloading(false);
     };
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="App">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
